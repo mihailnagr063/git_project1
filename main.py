36da48c154a507ae826057afadc131a1866f38b2
USG_411 <sadasd>
2023-11-17 17:17:25 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def my_superfunction():
+    print('Some print')
+
+
+def main():
+    print('print 1')
+    print('print 2')
+    print('print 3')
+    my_superfunction()
+
+
+if __name__ == '__main__':
+    main()
